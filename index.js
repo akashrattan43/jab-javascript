e90b3a35127bfcb2290468e0343bc29220a52a81
akashrattan43 <akashrattan43@gmail.com>
2020-12-22 19:35:04 -0800

    object

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+const car = null;
+//
+//symbol
+const sym = Symbol()
+
+//reference 
+// array
+const hobbies = ['movies', 'music']
+//object
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+console.log(typeof address)
