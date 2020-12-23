6242a5334556857687aa66812fe059069495a5c9
akashrattan43 <akashrattan43@gmail.com>
2020-12-22 19:42:42 -0800

    Math

--- index.js
+++ index.js
@@ -13,4 +13,20 @@ const address = {
     city: 'Boston',
     state: 'MA'
 }
-console.log(typeof address)
+let val;
+
+val = Math.PI
+val = Math.E
+val = Math.round(2.5)
+val = Math.ceil(2.4)
+val = Math.floor(2.8)
+val = Math.sqrt(64)
+val = Math.abs(-3)
+val = Math.pow(8, 2)
+val = Math.min(2, 33, 4, 1, 55, 6, 3)
+val = Math.max(1,2, 234, 345, 645)
+val = Math.random()
+
+val = Math.floor(Math.random() * 100 + 0)
+
+console.log(val)
