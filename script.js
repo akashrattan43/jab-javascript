8628bb6151ccacc5ae608b5bc45ffad56ce9eb2b
akash rattan <akashrattan43@Gmail.com>
2021-01-11 13:08:58 -0800

    this

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 // let happy = true
 
-
 // console.log(!happy)
 
-let a = 1
-let b = 2
+// let a = 1
+// let b = 2
 
-console.log(a == b)
+// console.log(a == b)
+
+// const ak = {
+//     name: 'Ak',
+//     age: 30
+// }
+
+// console.log(this.name)
+
+//person constructor
+
+// function Person(name, dob) {
+//   this.name = name;
+//   this.birthday = new Date(dob);
+//   this.calculateAge = function () {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   };
+// }
+
+// //if trigger in global it will refer to global object
+
+// // const brad = new Person('holla')
+// // const hamy = new Person('hamy')
+
+// // console.log(brad)
+
+// const ak = new Person("Ak", "12-27-1996");
+
+// console.log(ak.calculateAge());
