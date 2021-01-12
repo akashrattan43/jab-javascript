f8c38f1438e92bc0b6b2bb258341b581c07472bb
akash rattan <akashrattan43@Gmail.com>
2021-01-11 18:18:28 -0800

    prototype

--- script.js
+++ script.js
@@ -36,3 +36,44 @@
 // const ak = new Person("Ak", "12-27-1996");
 
 // console.log(ak.calculateAge());
+
+// object prototype
+//person.prototype
+
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  //   this.calculateAge = function () {
+  //     const diff = Date.now() - this.birthday.getTime();
+  //     const ageDate = new Date(diff);
+  //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+  //   };
+}
+
+//calculate age
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+//get full name
+
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+// gets married 
+Person.prototype.getsMarried = function(newLastName){
+  this.lastName = newLastName
+}
+
+const john = new Person("john", "doe", "8-12-90");
+const mary = new Person("mary", "jane", "9-8-2000");
+
+console.log(mary.calculateAge());
+console.log(mary.getFullName());
+
+mary.getsMarried('smith')
+console.log(mary.getFullName());
