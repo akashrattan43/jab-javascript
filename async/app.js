8b50092d0f6071e5d8200d26f7c7840c99cb63b7
akash rattan <akashrattan43@Gmail.com>
2021-01-14 19:59:01 -0800

    jokes

--- async/app.js
+++ async/app.js
@@ -1,16 +1,31 @@
-document.getElementById("button").addEventListener("click", loadData);
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
-function loadData() {
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+
   const xhr = new XMLHttpRequest();
 
-  //OPEN
-  xhr.open("GET", "data.txt", true);
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function () {
     if (this.status === 200) {
-      console.log(this.responseText);
+      const response = JSON.parse(this.responseText);
+
+      let output = "";
+
+      if (response.type === "success") {
+        response.value.forEach(function (joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += `<li>Something went wrong</li>`;
+      }
+
+      document.querySelector(".jokes").innerHTML = output;
     }
   };
 
   xhr.send();
+
+  e.preventDefault();
 }
